78b317bf98b5aaace32c0a18763973d3226710b0
Manasi Acharya <maanasee341@gmail.com>
2025-08-18 23:58:36 +0545

    Learning JavaScript

---
+++ variables.js
@@ -0,0 +1,28 @@
+// variable
+let firstName;
+firstName = 'Manasi';
+let lastName = 'Acharya';
+console.log(firstName, lastName);
+
+// constant
+const BirthDate = '2004';
+console.log(BirthDate);
+
+// datatypes
+// strings
+let favouriteFruit = 'Mango';
+let favouriteIcecream = "BlueBerry";
+let favouriteProgrammingLanguage = `JavaScript`
+
+//numbers
+let numberofDonuts = 12;
+let pi = 3.14;
+let largeNo = 127473889438n;
+
+//boolean
+let lovescoding = true;
+
+//undefined datatype
+let favouriteColor;
+console.log(favouriteColor);
+
